78137a0ae56ca4fdbc88107c8d3cdf2da473591b
MarcMG <marc.moli20@gmail.com>
2026-01-19 21:30:06 +0100

    Apply order of players when revealing the two first cards

--- game.py
+++ game.py
@@ -14,7 +14,29 @@ class Game:
         for player in self.players:
             player.reveal_card(0, 0)
             player.reveal_card(1, 1)
+        
+        self.determine_starting_player()
 
+    # Show player's Cards
     def display(self):
         for player in self.players:
             player.display()
+
+    # Determine order to play the game
+    def determine_starting_player(self):
+        # Calculate initial sum of every player
+        sums = []
+        for player in self.players:
+            sums.append((player, player.initial_revealed_sum()))
+
+        # Select player with highest result
+        starting_player, max_sum = max(sums, key=lambda x: x[1])
+
+        print(f"\nEmpieza el Jugador {starting_player.number} (suma inicial de : {max_sum})")
+
+        self.reorder_players(starting_player)
+
+    # Reordenate player's list
+    def reorder_players(self, starting_player):
+        index = self.players.index(starting_player)
+        self.players = self.players[index:] + self.players[:index]
